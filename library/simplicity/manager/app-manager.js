f6370420550133672dc00c3758cf0ddfc64aeecb
Patrick Bittner <anjunar@gmx.de>
2021-12-03 12:55:35 +0100

    bugfixing

--- library/simplicity/manager/app-manager.js
+++ library/simplicity/manager/app-manager.js
@@ -13,6 +13,15 @@ let language = "en";
 
 export const appManager = new class AppManager {
 
+    constructor() {
+        language = navigator.language.split("-")[0]
+        if (language === "en" || language === "de") {
+            // No Op
+        } else {
+            language = "en"
+        }
+    }
+
     get language() {
         return language;
     }
