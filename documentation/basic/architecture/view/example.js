d53ad39b4e8de717b7da60be9b6a236a9c2c1cf2
Patrick Bittner <anjunar@gmx.de>
2022-07-11 10:39:14 +0200

    bugfix

--- documentation/basic/architecture/view/example.js
+++ documentation/basic/architecture/view/example.js
@@ -1,5 +1,6 @@
 import {customViews} from "../../../../library/simplicity-core/simplicity.js";
 import {loader} from "../../../../library/simplicity-core/processors/loader-processor.js";
+import {appManager} from "../../../../library/simplicity-core/manager/app-manager.js";
 
 class Example extends HTMLElement {
 
@@ -16,7 +17,7 @@ export default customViews.define({
     class : Example,
     guard(activeRoute) {
         return {
-            material : fetch("materials.json")
+            material : fetch("/" + appManager.context + "/materials.json")
                 .then(response => response.json())
                 .then((materials) => {
                     return materials.rows.find((material) => material.position === Number.parseInt(activeRoute.queryParams.id))
