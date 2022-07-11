4385ca6acdcb86036a2d991e27a6a0342ee4191f
Patrick Bittner <anjunar@gmx.de>
2022-07-11 09:39:56 +0200

    bugfix

--- library/simplicity-core/processors/loader-processor.js
+++ library/simplicity-core/processors/loader-processor.js
@@ -14,7 +14,7 @@ export function loader(url) {
 export function libraryLoader(url) {
     let result;
     let request = new XMLHttpRequest();
-    request.open("GET", "/" + appManager.library + "/" + url, false)
+    request.open("GET", "" + appManager.context + "/" + appManager.library + "/" + url, false)
     request.addEventListener("loadend", (event) => {
         result = event.target.responseText
     })
